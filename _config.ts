f815fb3b7c0fe0200fd7d32e46f4b2e66071a271
Simon Holloway <simon@syhol.io>
2023-09-27 16:52:10 +0100

    custom github workflow for pages

--- _config.ts
+++ _config.ts
@@ -2,7 +2,8 @@ import lume from "lume/mod.ts";
 import basePath from "lume/plugins/base_path.ts";
 
 const site = lume({
-  dest: "docs",
+  src: "site",
+  dest: "site-dist",
   location: new URL("https://syhol.github.io/deno-stdlib-review/"), // ← Note the path /blog/
 });
 
