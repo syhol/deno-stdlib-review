2ec7cfefb9841e8d5603e92383780b6b39ed25a3
Simon Holloway <simon@syhol.io>
2023-09-27 16:18:11 +0100

    fix site paths again

--- _config.ts
+++ _config.ts
@@ -1,8 +1,11 @@
 import lume from "lume/mod.ts";
+import basePath from "lume/plugins/base_path.ts";
 
 const site = lume({
   dest: "docs",
   location: new URL("https://syhol.github.io/deno-stdlib-review/"), // ← Note the path /blog/
 });
+
+site.use(basePath());
 
 export default site;
